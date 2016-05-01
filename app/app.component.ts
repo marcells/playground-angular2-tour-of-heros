2da9985e345c51613669af821decd57f4b910df7
Marcell Spies <marcells@gmx.de>
2016-05-01 23:48:35 +0200

    Added routing.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,15 +1,23 @@
 import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 import {HeroesComponent} from './heroes.component';
 import {HeroService} from './hero.service';
+
+@RouteConfig([{
+  path: '/heroes',
+  name: 'Heroes',
+  component: HeroesComponent
+}])
 
 @Component({
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <my-heroes></my-heroes>
+    <a [routerLink]="['Heroes']">Heroes</a>
+    <router-outlet>
   `,
-  directives: [HeroesComponent],
-  providers: [HeroService]
+  directives: [ROUTER_DIRECTIVES],
+  providers: [ROUTER_PROVIDERS, HeroService]
 })
 export class AppComponent {
   title = 'Tour of Heroes'
