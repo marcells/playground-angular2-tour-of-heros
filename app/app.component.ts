1e4f68f11afde4470b6e723a0149d9f9efb83f05
Marcell Spies <marcells@gmx.de>
2016-05-01 23:56:36 +0200

    Added dashboard and integrated it into routing.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,19 +1,29 @@
 import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 import {HeroesComponent} from './heroes.component';
+import {DashboardComponent} from './dashboard.component';
 import {HeroService} from './hero.service';
 
 @RouteConfig([{
   path: '/heroes',
   name: 'Heroes',
   component: HeroesComponent
+},
+{
+  path: '/dashboard',
+  name: 'Dashboard',
+  component: DashboardComponent,
+  useAsDefault: true
 }])
 
 @Component({
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <a [routerLink]="['Heroes']">Heroes</a>
+    <nav>
+      <a [routerLink]="['Dashboard']">Dashboard</a>
+      <a [routerLink]="['Heroes']">Heroes</a>
+    </nav>
     <router-outlet>
   `,
   directives: [ROUTER_DIRECTIVES],
